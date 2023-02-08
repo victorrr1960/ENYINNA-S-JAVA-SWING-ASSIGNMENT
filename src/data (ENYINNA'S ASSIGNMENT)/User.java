44009a3b4658f746cd7001c70e36217da81fc77e
ENYINNA OGUERI <123371270+victorrr1960@users.noreply.github.com>
2023-02-08 21:31:53 +0100

    Add files via upload

---
+++ src/data (ENYINNA'S ASSIGNMENT)/User.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Enyinna
+ */
+public class User {
+
+    String firstName;
+    String lastName;
+    String city;
+    String age;
+    String Telephonenumber;
+    String Email;
+    String Address;
+    String Country;
+    String Major;
+    Icon  Photo;
+   
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTelephonenumber() {
+        return Telephonenumber;
+    }
+
+    public void setTelephonenumber(String Telephonenumber) {
+        this.Telephonenumber = Telephonenumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public Icon getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(Icon Photo) {
+        this.Photo = Photo;
+    }
+
+    
+    
+
+}
